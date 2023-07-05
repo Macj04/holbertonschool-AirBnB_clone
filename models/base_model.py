f1d37a1dca157cff6dc8d8d31068a9f470376c34
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-07-05 15:56:18 -0700

    Task 03

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel():
         """Init files function"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation"""
@@ -20,12 +20,12 @@ class BaseModel():
 
     def save(self):
         """Save data time"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Creation of new_dict"""
         new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['update_at'] = self.update_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['create_at'] = self.created_at.isoformat()
+        new_dict['update_at'] = self.updated_at.isoformat()
         return new_dict
