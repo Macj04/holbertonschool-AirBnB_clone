bac72c2df7b2e503e453b0e29f8ce5cbb221f042
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-07-07 16:59:51 -0300

    Task 06

--- console.py
+++ console.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """command line interpreter"""
-
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return True
+        quit()
 
     def do_EOF(self, arg):
         """To exit the program with Ctrl+C or EOF"""
         print()
-        return True
+        quit()
 
     def emptyline(self):
         """An empty line if no input"""
