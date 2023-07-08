f2bc0aae36f3fc3f2278dfacf5e7987dfbca3143
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-07-08 09:30:18 -0300

    Task 06 - Console

--- console.py
+++ console.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-"""command line interpreter"""
-
+""" Console """
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """
-    Command line interpreter for HBNB.
-    """
-
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program
-        """
-        quit()
+        """Quit the program"""
+        return True
 
     def do_EOF(self, arg):
-        """
-        Exit the program with Ctrl+D or EOF.
-        """
-        print()
-        quit()
+        """When user use Ctrl+D"""
+        return True
 
     def emptyline(self):
-        """
-        An empty line if no input.
-        """
+        """New line at empty line"""
         pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
